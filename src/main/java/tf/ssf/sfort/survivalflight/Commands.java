6b91f54662d5544cd9a3be819211e2c6cb00ff84
SFort <github@ssf.tf>
2021-10-28 15:46:35 +0200

    v1.1.5 change command syntax and add ui command

--- src/main/java/tf/ssf/sfort/survivalflight/Commands.java
+++ src/main/java/tf/ssf/sfort/survivalflight/Commands.java
@@ -1,24 +1,37 @@
 package tf.ssf.sfort.survivalflight;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.server.command.ServerCommandSource;
-import tf.ssf.sfort.survivalflight.mixin.MixinConfig;
 
 public class Commands {
     public static void register(){
         try {
             CommandRegistrationCallback.EVENT.register((dispatcher, dedi) -> {
-                LiteralArgumentBuilder<ServerCommandSource> ssf = LiteralArgumentBuilder.literal("ssf");
-                dispatcher.register(ssf.then(LiteralArgumentBuilder.<ServerCommandSource>literal("load")
-                                .then(LiteralArgumentBuilder.<ServerCommandSource>literal("survivalflight")
+                LiteralArgumentBuilder<ServerCommandSource> ssf = LiteralArgumentBuilder.literal("survivalflight");
+                dispatcher.register(ssf.then(LiteralArgumentBuilder.<ServerCommandSource>literal("reload")
                                         .requires(pred -> pred.hasPermissionLevel(2))
                                         .executes((c) -> {
                                             Config.resetsettings();
                                             Config.reload_settings();
                                             return 1;
-                                        }))));
+                                        })));
             });
         }catch (Exception ignore){}
+    }
+    public static void registerClient(){
+        try {
+            LiteralArgumentBuilder<FabricClientCommandSource> command = LiteralArgumentBuilder.literal("survivalflight:client");
+            command.then(LiteralArgumentBuilder.<FabricClientCommandSource>literal("ui").executes(c -> {
+                MinecraftClient.getInstance().send(()->MinecraftClient.getInstance().setScreen(new ConfigScreen(null)));
+                return 1;
+            }));
+            ClientCommandManager.DISPATCHER.register(command);
+        }catch (Exception igore){
+            System.out.println("\n\n\n"+igore);
+        }
     }
 }
